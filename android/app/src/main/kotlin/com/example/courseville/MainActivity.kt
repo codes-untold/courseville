e3ba79cf44ec49abcfdfc9e4c47ed9555b0330dc
codes-untold <talentedsoul74@gmail.com>
2021-07-22 16:03:09 -0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/courseville/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.courseville
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:courseville/TestWidget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TestWidget(),
+  ));
+}
+
