3a445db69f84f9de773c70ba0dfe5c9702ae1d3e
codes-untold <talentedsoul74@gmail.com>
2021-09-13 21:19:40 -0700

    update

---
+++ lib/Services/PaymentConstants.dart
@@ -0,0 +1,6 @@
+class PaymentConstants{
+
+  static const String PUBLIC_KEY = "FLWPUBK_TEST-7f782282dfb351547b3afdc7a71e7bbf-X";
+  static const String ENCRYPTION_KEY =  "FLWSECK_TEST64bb089f5851";
+
+}
